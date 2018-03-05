2f62bbb82a6f65355023ab21807f676b64baff00
Errif <36007073+Errif@users.noreply.github.com>
2018-03-05 17:42:18 +0100

    Simulation Update

--- Projects/Population_Growth/creature.js
+++ Projects/Population_Growth/creature.js
@@ -1,13 +1,13 @@
 class Creature {
 
-  constructor(x, y, size) {
+  constructor(x, y, size, cColor) {
     this.xPos = x;
     this.yPos = y;
     this.size = size;
     this.age = 0;
 
     this.canReproduce = false;
-    this.creatureColor = color(0, 100, 100)
+    this.creatureColor = cColor;
   }
 
   show() {
@@ -20,32 +20,46 @@ class Creature {
 
   ageCreature() {
     this.age++;
+    this.size = this.size * 1.5;
+
 
     if (this.age > 1) {
       this.canReproduce = true;
-      console.log("Reached maturity");
+      //console.log("Reached maturity");
     }
 
   }
 
   reproduce() {
-    console.log("Reproducing...");
+    //console.log("Reproducing...");
 
     // Create new creatures
     let numNewCreatures = round(random(1, 4));
     let newCreatures = [];
     for (let i = 0; i < numNewCreatures; i++) {
-      //let v = p5.Vector.random2D();
-      let x = random(-this.size * 2, this.size * 2);
-      let y = random(-this.size * 2, this.size * 2);
 
-      let newCreature = new Creature(this.xPos + 20 * random([-1, 1]), this.yPos + 20 * random([-1, 1]), this.size);
-      newCreature.setCreatureColor(color(0, 100, 150))
+      let x = constrain(this.xPos + 20 * random([-1, 0, 1]), 20, windowWidth - 20);
+      let y = constrain(this.yPos + 20 * random([-1, 0, 1]), 20, windowHeight - 20);
+
+
+      let ry = round(random(1, 10));
+
+
+      let r = constrain(this.creatureColor.levels[0] + random([-ry, 0, ry]), 0, 255);
+      let g = constrain(this.creatureColor.levels[1] + random([-ry, 0, ry]), 0, 255);
+      let b = constrain(this.creatureColor.levels[2] + random([-ry, 0, ry]), 0, 255);
+
+      let newColor = color(r, g, b);
+
+
+
+
+      let newCreature = new Creature(x, y, 10, newColor);
+      //newCreature.setCreatureColor(color(0, 100, 150))
       newCreatures.push(newCreature);
 
     }
     return newCreatures;
-    // TODO: Add reproductiong feature for creatures
   }
 
   setCreatureColor(newColor) {
--- Projects/Population_Growth/sketch.js
+++ Projects/Population_Growth/sketch.js
@@ -1,7 +1,7 @@
 const FRAMERATE = 60;
 
 // Simulation Settings
-let secondsPerYear = 2;
+let secondsPerYear = 0.01;
 
 // Population Settings
 let startPopulationSize = 10;
@@ -12,7 +12,7 @@ let nextYearTime = 0;
 let elapsedSeconds = 0;
 
 //
-let creatureMaxAge = 3;
+let creatureMaxAge = 6;
 let creatures = []
 
 function setup() {
@@ -21,9 +21,12 @@ function setup() {
   nextYearTime = secondsPerYear;
   //creatures.push(new Creature(100,100,20))
 
+  let creatureColors = [color('#aabf12'), color('#33ab12'), color('#165512'), color('#fe3fa2'), color('#a345cd'), color('#a345cd')];
+
   // Starting Creatures
   for (var i = 0; i < 6; i++) {
-    let newCreature = new Creature(random(200, 600), random(200, 600), random(20, 60));
+    let col = creatureColors[i]
+    let newCreature = new Creature(random(200, 600), random(200, 600), random(20, 60), col);
     creatures.push(newCreature)
 
   }
@@ -68,7 +71,11 @@ function newYear() {
   for (let c of creatures) {
     let r = random();
 
-    if (r < 0.50) {
+    if (c.age == 1 && r < 0.50) {
+      c.die();
+      let i = creatures.indexOf(c);
+      creatures.splice(i, 1);
+    } else if (c.age == 2 && r < 0.25) {
       c.die();
       let i = creatures.indexOf(c);
       creatures.splice(i, 1);
@@ -77,7 +84,6 @@ function newYear() {
 
   // Update creatures
   for (let c of creatures) {
-    let r = random();
 
     if (c.age > creatureMaxAge) {
       c.die();
@@ -85,7 +91,9 @@ function newYear() {
       creatures.splice(i, 1);
     }
 
-    if (c.canReproduce && creatures.length < 1000) {
+
+
+    if (c.canReproduce && creatures.length < 2000) {
       let bornCreatures = c.reproduce();
       creatures = creatures.concat(bornCreatures);
     }
